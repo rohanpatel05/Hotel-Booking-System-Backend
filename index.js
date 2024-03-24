46d65bb7fa4dd532f96769a3b6212517f8e1146e
Rohan Patel <rohangckp@gmail.com>
2024-03-24 13:32:53 -0500

    Corrected naming convention.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import connectDB from './config/db.js';
 import cors from 'cors';
-import userRoutes from './routes/users.js'
+import userRoutes from './routes/userRoutes.js'
 
 
 const app = express(); 

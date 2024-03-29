ae38bf794b27ee9cd7f51c2c8a7410d047764f8f
Rohan Patel <rohangckp@gmail.com>
2024-03-29 15:35:55 -0500

    Updated server.

--- index.js
+++ index.js
@@ -32,27 +32,29 @@ app.use(baseUrl, bookingRoutes);
 app.use(baseUrl, paymentRoutes);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 8080;
-const server = app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-process.on('SIGINT', () => {
-  console.log('Received SIGINT. Closing server...');
-  server.close(() => {
-      console.log('Server closed');
-      disconnectDB();
-      process.exit(0);
+if (process.env.NODE_ENV !== 'test') {
+  const port = process.env.PORT || 8080;
+  const server = app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
   });
-});
 
-process.on('SIGTERM', () => {
-  console.log('Received SIGTERM. Closing server...');
-  server.close(() => {
-      console.log('Server closed');
-      disconnectDB(); 
-      process.exit(0);
+  process.on('SIGINT', () => {
+    console.log('Received SIGINT. Closing server...');
+    server.close(() => {
+        console.log('Server closed');
+        disconnectDB();
+        process.exit(0);
+    });
   });
-});
+
+  process.on('SIGTERM', () => {
+    console.log('Received SIGTERM. Closing server...');
+    server.close(() => {
+        console.log('Server closed');
+        disconnectDB(); 
+        process.exit(0);
+    });
+  });
+}
 
 export default app;

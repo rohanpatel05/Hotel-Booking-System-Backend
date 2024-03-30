5ededd44e44c92fbed481e74287f6004806bd277
Rohan Patel <rohangckp@gmail.com>
2024-03-29 19:08:06 -0500

    Updated route to indlude middlewears.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,28 @@
-import express from 'express';
-import userController from '../controllers/userControllers.js';
+import express from "express";
+import userController from "../controllers/userControllers.js";
+import userExistMiddleware from "../middlewares/userExistMiddlewear.js";
+import userAuthMiddleware from "../middlewares/userAuthMiddlewear.js";
 
 const router = express.Router();
 
-let baseUserUrl = '/user'
+let baseUserUrl = "/user";
 
-router.post('/signup', userController.signup);
-router.post('/login', userController.login);
-router.get('/logout', userController.logout);
-router.put('/refresh', userController.refresh);
+router.post("/signup", userController.signup);
+router.post("/login", userController.login);
+router.get("/logout", userAuthMiddleware, userController.logout);
+router.put("/refresh", userController.refresh);
 
 // User profile routes
-router.put(baseUserUrl + '/updateProfile/:userId', userController.updateUserProfile);
-router.put(baseUserUrl + '/changePassword/:userId', userController.changePassword);
+router.put(
+  baseUserUrl + "/updateProfile/:userId",
+  userAuthMiddleware,
+  userController.updateUserProfile
+);
+router.put(
+  baseUserUrl + "/changePassword/:userId",
+  userExistMiddleware,
+  userAuthMiddleware,
+  userController.changePassword
+);
 
 export default router;

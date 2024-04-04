cef12fea7e8c6503c6a7d3f534649fb622c152c0
Rohan Patel <rohangckp@gmail.com>
2024-04-04 14:33:41 -0500

    Server alternative port changed.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./app.js";
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });

b0a3d14c09e5b79bc6c08568a5a8a2b8ba8d72f6
Rohan Patel <rohangckp@gmail.com>
2024-04-06 18:42:35 -0500

    Updated room api endpoint routes format.

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import roomController from '../controllers/roomControllers.js';
+import express from "express";
+import roomController from "../controllers/roomControllers.js";
 
 const router = express.Router();
 
-let baseRoomURL = '/room';
+let baseRoomURL = "/room";
 
-router.get(baseRoomURL + '/', roomController.getAllRooms);
-router.get(baseRoomURL + '/:id', roomController.getRoomById);
-router.post(baseRoomURL + '/create', roomController.createRoom);
-router.put(baseRoomURL + '/:id/update', roomController.updateRoom);
-router.delete(baseRoomURL + '/:id/delete', roomController.deleteRoom);
+router.get(baseRoomURL + "/", roomController.getAllRooms);
+router.get(baseRoomURL + "/:userId", roomController.getRoomById);
+router.post(baseRoomURL + "/create", roomController.createRoom);
+router.put(baseRoomURL + "/update/:userId", roomController.updateRoom);
+router.delete(baseRoomURL + "/delete/:userId", roomController.deleteRoom);
 
 export default router;

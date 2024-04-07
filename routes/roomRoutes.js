de81f166366df35a344be26cb87233210b19c163
Rohan Patel <rohangckp@gmail.com>
2024-04-07 10:38:18 -0500

    Endpoint route format updated.

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 let baseRoomURL = "/room";
 
 router.get(baseRoomURL + "/", roomController.getAllRooms);
-router.get(baseRoomURL + "/:userId", roomController.getRoomById);
+router.get(baseRoomURL + "/byId/:userId", roomController.getRoomById);
 router.post(baseRoomURL + "/create", roomController.createRoom);
 router.put(baseRoomURL + "/update/:userId", roomController.updateRoom);
 router.delete(baseRoomURL + "/delete/:userId", roomController.deleteRoom);
